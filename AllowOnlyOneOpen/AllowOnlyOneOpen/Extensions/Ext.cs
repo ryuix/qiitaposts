71a997e910c9f0dc031421530d411af22ca302fc
ryuix <ryu1mizukami@gmail.com>
2021-03-09 00:54:56 +0900

    :sparkles: AllowOnlyOneTrue

---
+++ AllowOnlyOneOpen/AllowOnlyOneOpen/Extensions/Ext.cs
@@ -0,0 +1,27 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllowOnlyOneOpen.Extensions
+{
+    public static class Ext
+    {
+        public static IDisposable AllowOnlyOneTrue(this IEnumerable<IReactiveProperty<bool>> source)
+            => source
+            .CombineLatest()
+            .Pairwise()
+            .Where(x => x.NewItem.SkipWhile(n => !n).Skip(1).Any(n => n))
+            .Subscribe(p => p.OldItem
+                .Select((v, i) => (v, i))
+                .Where(x => x.v)
+                .Select(x => source.ElementAt(x.i).Value = false)
+                .ToList());
+    }
+}
---
+++ AllowOnlyOneOpen/AllowOnlyOneOpen/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AllowOnlyOneOpen.Extensions;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllowOnlyOneOpen.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ReactivePropertySlim<bool> AIsExpanded { get; } = new();
+        public ReactivePropertySlim<bool> BIsExpanded { get; } = new();
+        public ReactivePropertySlim<bool> CIsExpanded { get; } = new();
+        public ReactivePropertySlim<bool> DIsExpanded { get; } = new();
+
+
+        public MainWindowViewModel()
+        {
+            var group = new [] { AIsExpanded, BIsExpanded, CIsExpanded, DIsExpanded };
+
+            group.AllowOnlyOneTrue();
+
+        }
+    }
+}
